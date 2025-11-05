0b6bf9ac32cdd8bf46350f577cc54ee19816162a
charles.prouveur <charles.prouveur@gmail.com>
2025-11-05 14:20:23 +0100

    more elegant way to switch CPU/GPU acceleration with if inside the pragma ; Fix size issue by simply using present(pt1,pt2) without specifying the ranges in syncvectorpatch.cpp

--- src/Patch/SyncVectorPatch.cpp
+++ src/Patch/SyncVectorPatch.cpp
@@ -284,9 +284,9 @@ void SyncVectorPatch::sumAllComponents( std::vector<Field *> &fields, VectorPatc
                 const unsigned int last = gsp[0] * ny_ * nz_;
 
 #if defined( SMILEI_ACCELERATOR_GPU_OACC )
-                int ptsize = vecPatches.densitiesLocalx[ifield]->size();
-                int nspace0 = size[0];
-                #pragma acc parallel if ( is_memory_on_device) present(pt1[0-nspace0*ny_*nz_:ptsize],pt2[0:ptsize])
+                //int ptsize = vecPatches.densitiesLocalx[ifield]->size();
+                //int nspace0 = size[0];
+                #pragma acc parallel if ( is_memory_on_device) present(pt1,pt2)  //(pt1[0-nspace0*ny_*nz_:ptsize],pt2[0:ptsize])
                 #pragma acc loop worker vector
 #elif defined( SMILEI_ACCELERATOR_GPU_OMP )
                 #pragma omp target if( is_memory_on_device )
@@ -395,9 +395,9 @@ void SyncVectorPatch::sumAllComponents( std::vector<Field *> &fields, VectorPatc
                     const unsigned int inner_last   = gsp[1] * nz_;
 
 #if defined( SMILEI_ACCELERATOR_GPU_OACC )
-                    int ptsize = vecPatches.densitiesLocaly[ifield]->size();
-                    int blabla = size[1];
-                    #pragma acc parallel if (is_memory_on_device) present(pt1[0-blabla*nz_:ptsize],pt2[0:ptsize])
+                    //int ptsize = vecPatches.densitiesLocaly[ifield]->size();
+                    //int blabla = size[1];
+                    #pragma acc parallel if (is_memory_on_device) present(pt1,pt2)  //(pt1[0-blabla*nz_:ptsize],pt2[0:ptsize])
                     #pragma acc loop worker vector
 #elif defined( SMILEI_ACCELERATOR_GPU_OMP )
                     #pragma omp target if( is_memory_on_device )
@@ -508,13 +508,13 @@ void SyncVectorPatch::sumAllComponents( std::vector<Field *> &fields, VectorPatc
                         const unsigned int inner_last   = gsp[2];
 
 #if defined( SMILEI_ACCELERATOR_GPU_OACC )
-                        int ptsize = vecPatches.densitiesLocalz[ifield]->size();
-                        int blabla = size[2];
-                        #pragma acc parallel if (is_memory_on_device) present(pt1[0-blabla:ptsize],pt2[0:ptsize])
+                        //int ptsize = vecPatches.densitiesLocalz[ifield]->size();
+                        //int blabla = size[2];
+                        #pragma acc parallel if (is_memory_on_device) present(pt1,pt2)  //(pt1[0-blabla:ptsize],pt2[0:ptsize])
                         #pragma acc loop worker vector
 #elif defined( SMILEI_ACCELERATOR_GPU_OMP )
                         #pragma omp target if( is_memory_on_device )
-                         #pragma omp teams distribute parallel for collapse( 2 )
+                        #pragma omp teams distribute parallel for collapse( 2 )
 #endif
                         for( unsigned int j = 0; j < outer_last; j += outer_stride ) {
                             for( unsigned int i = 0; i < inner_last; i++ ) {
@@ -1148,40 +1148,25 @@ void SyncVectorPatch::exchangeSynchronizedPerDirection( std::vector<Field *> fie
                 field2 = static_cast<F *>( fields[ipatch] );
                 pt1 = &( *field1 )( size[2] );
                 pt2 = &( *field2 )( 0 );
-                if (is_memory_on_device){
 #ifdef SMILEI_ACCELERATOR_GPU_OACC
-                    int ptsize = vecPatches.B2_localz[ipatch]->size();
-                    int size2 = size[2];
-                    #pragma acc parallel present(pt1[0-size2:ptsize],pt2[0:ptsize])
-                    #pragma acc loop gang worker vector
+                //int ptsize = vecPatches.B2_localz[ipatch]->size();
+                //int size2 = size[2];
+                #pragma acc parallel if(is_memory_on_device) present(pt1,pt2)  //(pt1[0-size2:ptsize],pt2[0:ptsize])
+                #pragma acc loop gang worker vector
 #elif defined( SMILEI_ACCELERATOR_GPU_OMP )
-                    const int ptsize = ( nx_ * ny_ * nz_ ) - ( ny_ * nz_ ) + ( ny_ * nz_ ) - nz_ + oversize[2];
-                    #pragma omp target
-                    #pragma omp teams distribute parallel for collapse( 3 )
+                //const int ptsize = ( nx_ * ny_ * nz_ ) - ( ny_ * nz_ ) + ( ny_ * nz_ ) - nz_ + oversize[2];
+                #pragma omp target if(is_memory_on_device)
+                #pragma omp teams distribute parallel for collapse( 3 )
 #endif
-                    //for (unsigned int in = oversize[0] ; in < nx_-oversize[0]; in ++){
-                    for( unsigned int in = 0 ; in < nx_ ; in ++ ) {
-                        unsigned int i = in * ny_*nz_;
-                        //for (unsigned int jn = oversize[1] ; jn < ny_-oversize[1] ; jn ++){
-                        for( unsigned int jn = 0 ; jn < ny_ ; jn ++ ) {
-                            unsigned int j = jn *nz_;
-                            for( unsigned int k = 0 ; k < oversize[2] ; k++ ) {
-                                pt2[i+j+k] = pt1[i+j+k] ;
-                                pt1[i+j+k+gsp[2]] = pt2[i+j+k+gsp[2]] ;
-                            }
-                        }
-                    }
-                }else{  // cpu communication sync since no device pointer
-                    //for (unsigned int in = oversize[0] ; in < nx_-oversize[0]; in ++){
-                    for( unsigned int in = 0 ; in < nx_ ; in ++ ) {
-                        unsigned int i = in * ny_*nz_;
-                        //for (unsigned int jn = oversize[1] ; jn < ny_-oversize[1] ; jn ++){
-                        for( unsigned int jn = 0 ; jn < ny_ ; jn ++ ) {
-                            unsigned int j = jn *nz_;
-                            for( unsigned int k = 0 ; k < oversize[2] ; k++ ) {
-                                pt2[i+j+k] = pt1[i+j+k] ;
-                                pt1[i+j+k+gsp[2]] = pt2[i+j+k+gsp[2]] ;
-                            }
+                //for (unsigned int in = oversize[0] ; in < nx_-oversize[0]; in ++){
+                for( unsigned int in = 0 ; in < nx_ ; in ++ ) {
+                    unsigned int i = in * ny_*nz_;
+                    //for (unsigned int jn = oversize[1] ; jn < ny_-oversize[1] ; jn ++){
+                    for( unsigned int jn = 0 ; jn < ny_ ; jn ++ ) {
+                        unsigned int j = jn *nz_;
+                        for( unsigned int k = 0 ; k < oversize[2] ; k++ ) {
+                            pt2[i+j+k] = pt1[i+j+k] ;
+                            pt1[i+j+k+gsp[2]] = pt2[i+j+k+gsp[2]] ;
                         }
                     }
                 }
@@ -1235,35 +1220,23 @@ void SyncVectorPatch::exchangeSynchronizedPerDirection( std::vector<Field *> fie
                 field2 = static_cast<F *>( fields[ipatch] );
                 pt1 = &( *field1 )( size[1]*nz_ );
                 pt2 = &( *field2 )( 0 );
-                if (is_memory_on_device){
 #ifdef SMILEI_ACCELERATOR_GPU_OACC
-                    int ptsize = vecPatches.B1_localy[ipatch]->size();
-                    int size1 = size[1];
-                    #pragma acc parallel present(pt1[0-size1*nz_:ptsize],pt2[0:ptsize])
-                    #pragma acc loop gang worker vector
+                //int ptsize = vecPatches.B1_localy[ipatch]->size();
+                //int size1 = size[1];
+                #pragma acc parallel if(is_memory_on_device) present(pt1[0-size1*nz_:ptsize],pt2[0:ptsize])//present(pt1,pt2)  //(pt1[0-size1*nz_:ptsize],pt2[0:ptsize])//-size1*nz_
+                #pragma acc loop gang worker vector
 #elif defined( SMILEI_ACCELERATOR_GPU_OMP )
-                    const int ptsize = ( nx_ * ny_ * nz_ ) - ( ny_ * nz_ ) + oversize[1] * nz_ + gsp[1] * nz_;
-                    #pragma omp target
-                    #pragma omp teams distribute parallel for collapse( 2 )
+                //const int ptsize = ( nx_ * ny_ * nz_ ) - ( ny_ * nz_ ) + oversize[1] * nz_ + gsp[1] * nz_;
+                #pragma omp target if(is_memory_on_device)
+                #pragma omp teams distribute parallel for collapse( 2 )
 #endif
-                    for( unsigned int in = 0 ; in < nx_ ; in ++ ) {
-                        //for (unsigned int in = oversize[0] ; in < nx_-oversize[0] ; in ++){ // <== This doesn't work. Why ??
-                        unsigned int i = in * ny_*nz_;
-                        for( unsigned int j = 0 ; j < oversize[1]*nz_ ; j++ ) {
-                            // Rewrite with memcpy ?
-                            pt2[i+j] = pt1[i+j] ;
-                            pt1[i+j+gsp[1]*nz_] = pt2[i+j+gsp[1]*nz_] ;
-                        }
-                    }
-                }else{  // cpu communication sync since no device pointer
-                    for( unsigned int in = 0 ; in < nx_ ; in ++ ) {
-                        //for (unsigned int in = oversize[0] ; in < nx_-oversize[0] ; in ++){ // <== This doesn't work. Why ??
-                        unsigned int i = in * ny_*nz_;
-                        for( unsigned int j = 0 ; j < oversize[1]*nz_ ; j++ ) {
-                            // Rewrite with memcpy ?
-                            pt2[i+j] = pt1[i+j] ;
-                            pt1[i+j+gsp[1]*nz_] = pt2[i+j+gsp[1]*nz_] ;
-                        }
+                for( unsigned int in = 0 ; in < nx_ ; in ++ ) {
+                    //for (unsigned int in = oversize[0] ; in < nx_-oversize[0] ; in ++){ // <== This doesn't work. Why ??
+                    unsigned int i = in * ny_*nz_;
+                    for( unsigned int j = 0 ; j < oversize[1]*nz_ ; j++ ) {
+                        // Rewrite with memcpy ?
+                        pt2[i+j] = pt1[i+j] ;
+                        pt1[i+j+gsp[1]*nz_] = pt2[i+j+gsp[1]*nz_] ;
                     }
                 }
             } // End if ( MPI_me_ == MPI_neighbor_[1][0] )
@@ -1518,7 +1491,8 @@ void SyncVectorPatch::exchangeAllComponentsAlongY( std::vector<Field *> &fields,
                 pt2 = &( vecPatches.B1_localy[ifield]->data_[0] );
 #ifdef SMILEI_ACCELERATOR_GPU_OACC
                 int ptsize = vecPatches.B1_localy[ifield]->size();
-                #pragma acc parallel present(pt1[0:ptsize],pt2[0:ptsize])
+                //int sizetnz = size*nz_;
+                #pragma acc parallel present(pt1,pt2)  //(pt1[0-size*nz_:ptsize],pt2[0:ptsize])
                 #pragma acc loop gang worker vector
 #elif defined( SMILEI_ACCELERATOR_GPU_OMP )
                 #pragma omp target
@@ -1627,7 +1601,7 @@ void SyncVectorPatch::exchangeAllComponentsAlongZ( std::vector<Field *> fields, 
                 pt2 = &( vecPatches.B2_localz[ifield]->data_[0] );
 #ifdef SMILEI_ACCELERATOR_GPU_OACC
                 int ptsize = vecPatches.B2_localz[ifield]->size();
-                #pragma acc parallel present(pt1[0:ptsize],pt2[0:ptsize])
+                #pragma acc parallel present(pt1,pt2)  //(pt1[0-size:ptsize],pt2[0:ptsize])
                 #pragma acc loop gang worker vector
 #elif defined( SMILEI_ACCELERATOR_GPU_OMP )
                 #pragma omp target
